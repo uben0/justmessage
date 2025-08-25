8a371afc95f5ab6ae92b09e2bf70578a96986c85
Eddie Gerbais-Nief <eddie.gerbais-nief@proton.me>
2025-08-25 17:00:03 +0200

    fixed webhook

--- fichar/src/main.rs
+++ fichar/src/main.rs
@@ -1,29 +1,39 @@
 use axum::{Router, extract::Request, http::StatusCode, routing::post};
 use axum_server::tls_rustls::RustlsConfig;
 use clap::Parser;
-use std::path::PathBuf;
+// use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Args {
-    #[arg(long)]
-    cert: PathBuf,
-    #[arg(long)]
-    key: PathBuf,
+    // #[arg(long)]
+    // cert: PathBuf,
+    // #[arg(long)]
+    // key: PathBuf,
     #[arg(long)]
     token: String,
 }
 
 #[tokio::main]
 async fn main() {
-    let Args { cert, key, token } = Args::parse();
+    dotenvy::dotenv().ok();
+    let token = std::env::var("JUSTMESSAGE_TELEGRAM_BOT_TOKEN").unwrap();
+    // let Args { token } = Args::parse();
+    let generated =
+        rcgen::generate_simple_self_signed(["fr1.justmessage.uben.ovh".to_string()]).unwrap();
+
     let response = telegram::set_webhook(&token, "https://fr1.justmessage.uben.ovh".into())
         .drop_pending_updates()
-        .certificate(std::fs::read(&cert).unwrap())
+        .certificate(generated.cert.pem().into())
         .send()
         .await
         .unwrap();
     println!("{response:#?}");
-    let tls_conf = RustlsConfig::from_pem_file(cert, key).await.unwrap();
+    let tls_conf = RustlsConfig::from_pem(
+        generated.cert.pem().into(),
+        generated.signing_key.serialize_pem().into(),
+    )
+    .await
+    .unwrap();
     let app = Router::new().route("/", post(handler));
     axum_server::bind_rustls(([0, 0, 0, 0], 443).into(), tls_conf)
         .serve(app.into_make_service())
